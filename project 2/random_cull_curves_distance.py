7b7fabbf8682a5c9fa7aac309195f22f77504d3b
Sam Ebertz <samuel.ebertz@gmail.com>
2017-02-15 01:39:23 -0600

    assignment 2 voronoi culling and projection in gh

---
+++ project 2/random_cull_curves_distance.py
@@ -0,0 +1,19 @@
+﻿#pseudo-random culling algorithm with variable probability based on distance to a control point
+import rhinoscriptsyntax as rs
+import random
+
+random.seed()
+def cull(pt, curves, s):
+    cull = []
+    for curve in curves:
+        if rs.CloseCurve(curve) == False:
+            print('invalid curve: not closed')
+        #calculate the distance from the control point to the centroid of the curve
+        d = rs.Distance(pt, rs.CurveAreaCentroid(curve)[0])
+        #scale culling probability by s (probably distance to an attractor point)
+        if(random.random() < 1 / (s * d)):
+            cull.append(curve)
+    for curve in cull:
+        curves.remove(curve)
+    return curves
+C = cull(P, C, S)
